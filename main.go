836c2cb135c4421984b58320da020cb4e8a73aa4
prasetyama hidayat <prasetyama@dailysocial.net>
2022-03-30 12:35:51 +0700

    import featured image wich no featured image

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"math/big"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -31,7 +31,7 @@ func dsn_ds(dbname_ds string) string {
 }
 
 func GetPosts(db *sql.DB) ([]models.Post, error) {
-	query := `select id, post_name, post_title from wp_posts where post_date BETWEEN '2020-01-01' AND '2020-01-31' 
+	query := `select id, post_title, post_name from wp_posts where post_date BETWEEN '2020-01-01' AND '2020-01-31' 
 	AND post_type = 'post' AND post_status = 'publish' order by id desc LIMIT 3;`
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
@@ -91,32 +91,50 @@ func GetPostsMeta(db *sql.DB, postId int) ([]models.Meta, error) {
 
 }
 
-func GetPostbyName(db *sql.DB, post_name string) ([]models.Post, error) {
-	query := `select * from wp_posts where post_name = ?`
+func GetPostbyName(db *sql.DB, post_name string) (int, error) {
+	query := `select id from wp_posts where post_name = ?`
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
 	stmt, err := db.PrepareContext(ctx, query)
 	if err != nil {
 		log.Printf("Error %s when preparing SQL statement", err)
-		return []models.Post{}, err
+		return 0, err
 	}
 	defer stmt.Close()
-	row, err := stmt.QueryContext(ctx, post_name)
+	var id int
+	row := stmt.QueryRowContext(ctx, post_name)
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func insert(db *sql.DB, meta models.Meta) error {
+	query := "INSERT INTO wp_postmeta(post_id, meta_key, meta_value) VALUES (?, ?, ?)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	stmt, err := db.PrepareContext(ctx, query)
 	if err != nil {
-		return []models.Post{}, err
+		log.Printf("Error %s when preparing SQL statement", err)
+		return err
 	}
-	defer row.Close()
-	var post = models.Post{}
-
-	if err := row.Scan(&post.Id, &post.Post_title, &post.Post_name); err != nil {
-		return []models.Post{}, err
+	defer stmt.Close()
+	res, err := stmt.ExecContext(ctx, meta.Post_id, meta.Meta_key, meta.Meta_value)
+	if err != nil {
+		log.Printf("Error %s when inserting row into meta post table", err)
+		return err
 	}
-	return []models.Post{}, nil
-
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when finding rows affected", err)
+		return err
+	}
+	log.Printf("%d meta post created ", rows)
+	return nil
 }
 
 func main() {
-	db, err := sql.Open("mysql", dsn(""))
+	db_trikinet, err := sql.Open("mysql", dsn(""))
 
 	db_ds, err_ds := sql.Open("mysql", dsn_ds(""))
 
@@ -124,39 +142,23 @@ func main() {
 		log.Printf("Error %s when opening DB\n", err)
 		return
 	}
-	defer db.Close()
+	defer db_trikinet.Close()
 
 	if err_ds != nil {
 		log.Printf("Error %s when opening DB\n", err_ds)
 		return
 	}
 	defer db_ds.Close()
-
-	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelfunc()
-	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname_trikinet)
-	if err != nil {
-		log.Printf("Error %s when creating DB\n", err)
-		return
-	}
-	no, err := res.RowsAffected()
-	if err != nil {
-		log.Printf("Error %s when fetching rows", err)
-		return
-	}
-	log.Printf("rows affected %d\n", no)
-
-	db.Close()
-	db, err = sql.Open("mysql", dsn(dbname_trikinet))
+	db_trikinet, err = sql.Open("mysql", dsn(dbname_trikinet))
 	if err != nil {
 		log.Printf("Error %s when opening DB", err)
 		return
 	}
-	defer db.Close()
+	defer db_trikinet.Close()
 
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(20)
-	db.SetConnMaxLifetime(time.Minute * 5)
+	db_trikinet.SetMaxOpenConns(20)
+	db_trikinet.SetMaxIdleConns(20)
+	db_trikinet.SetConnMaxLifetime(time.Minute * 5)
 
 	db_ds, err_ds = sql.Open("mysql", dsn(dbname_ds))
 	if err_ds != nil {
@@ -169,40 +171,85 @@ func main() {
 	db_ds.SetMaxIdleConns(20)
 	db_ds.SetConnMaxLifetime(time.Minute * 5)
 
-	posts, err := GetPosts(db)
+	posts, err := GetPosts(db_trikinet)
 	if err != nil {
 		log.Printf("Error %s when selecting post", err)
 		return
 	}
+
 	for _, post := range posts {
 
-		log.Printf("Post Title: %s Post Name: %d", post.Post_name, post.Post_title)
+		// log.Printf("Post Title: %s Post Name: %d", post.Post_name, post.Post_title)
 
-		posts_ds, err := GetPostbyName(db_ds, "cara-freeze-kolom-dan-baris-pertama-microsoft-excel")
+		posts_ds, err := GetPostbyName(db_ds, post.Post_name)
+
+		switch {
+		case err == sql.ErrNoRows:
+			log.Printf("Post %s not found in DB", post.Post_title)
+		case err != nil:
+			log.Printf("Encountered err %s when fetching post from DB", err)
+		default:
+			log.Printf("Post of %s is %d", post.Post_name, posts_ds)
+		}
+
+		post_meta_ds, err := GetPostsMeta(db_ds, int(posts_ds))
 
 		if err != nil {
-			log.Printf("Error %s when selecting post", err)
+			log.Printf("Error %s when selecting post meta", err)
 			return
 		}
 
-		for _, post_ds := range posts_ds {
+		for key, meta := range post_meta_ds {
+			// log.Printf("post id : %s, meta key : %q, Meta Value : %p", posts_ds, meta.Meta_key, &meta.Meta_value)
 
-			var postId = big.NewInt(post_ds.Id).String()
-			log.Printf("ID : %q Post Title: %s Post Name: %d", postId, post_ds.Post_name, post_ds.Post_title)
+			if meta.Meta_key == "_thumbnail_id" {
+
+				var _thumbnail_id_ds, err = strconv.Atoi(post_meta_ds[key].Meta_value)
+				// log.Printf("thumbnail id ds %s", _thumbnail_id_ds)
+				thumbnails_ds, err := GetPostsMeta(db_ds, _thumbnail_id_ds)
+				// log.Printf("thumbnail src : %s", thumbnails_ds)
+				// fmt.Println("thumbnail src ", thumbnails_ds)
+
+				if err != nil {
+					log.Printf("thumbnail src %s Not found", post.Post_title)
+					return
+				} else {
+					post_meta_trikinet, err := GetPostsMeta(db_trikinet, int(post.Id))
+
+					if err != nil {
+						log.Printf("Error %s when selecting post meta", err)
+						return
+					}
+
+					if len(thumbnails_ds) > 0 {
+						log.Printf("Meta Post %s already exist", post.Post_name)
+					} else {
+						var _thumbnail_id_trikinet int
+						for key, meta_trikinet := range post_meta_trikinet {
+							if meta_trikinet.Meta_key == "_thumbnail_id" {
+								_thumbnail_id_trikinet, err = strconv.Atoi(post_meta_trikinet[key].Meta_value)
+							}
+						}
+
+						thumbnails_trikinet, err := GetPostsMeta(db_trikinet, _thumbnail_id_trikinet)
+
+						for _, thumbnail_trikinet := range thumbnails_trikinet {
+							data := models.Meta{
+								Post_id:    int64(_thumbnail_id_ds),
+								Meta_key:   thumbnail_trikinet.Meta_key,
+								Meta_value: thumbnail_trikinet.Meta_value,
+							}
+							err = insert(db_ds, data)
+						}
+
+						if err != nil {
+							log.Printf("Error %s when selecting post meta", err)
+							return
+						}
+					}
+				}
+			}
 		}
-
-		// log.Printf("ID : %q Post Title: %s Post Name: %d", postId, post.Post_name, post.Post_title)
-
-		// post_meta, err := GetPostsMeta(db, int(post.Id))
-
-		// if err != nil {
-		// 	log.Printf("Error %s when selecting post meta", err)
-		// 	return
-		// }
-
-		// for _, meta := range post_meta {
-		// 	log.Printf("post id : %s, meta key : %q, Meta Value : %p", postId, meta.Meta_key, &meta.Meta_value)
-		// }
 
 	}
 }
